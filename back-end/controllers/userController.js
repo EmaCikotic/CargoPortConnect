2cdc70008c400843b9f027260b4eaae87563e4c1
ema.cikotic <emacikotic25@gmail.com>
2025-01-19 22:40:04 +0100

    typo

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -1,5 +1,5 @@
 const bcrypt = require("bcryptjs");
-const User = require("../models/user");
+const User = require("../models/users");
 const db = require("../config/database");
 
 exports.register = async (req, res) => {
