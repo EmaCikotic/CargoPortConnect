49bd049d7801688bd578f260f41f69c6e30580a4
EmaCikotic <emacikotic25@gmail.com>
2025-08-15 14:42:44 +0200

    Deployed

--- back-end/server.js
+++ back-end/server.js
@@ -6,7 +6,7 @@ const logger = require("morgan");
 const cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 3002;
 
 // Middleware
 app.use(logger("dev"));
@@ -28,7 +28,11 @@ app.use(
 //app.use(cors({ origin: "http://localhost:3001", credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 // Import Routes
 const indexRouter = require("./routes/index");
--- front-end/src/Configuration.js
+++ front-end/src/Configuration.js
@@ -1,1 +1,4 @@
-export const API_URL = "http://localhost:5001";
+//export const API_URL = "http://localhost:5001";
+
+export const API_URL = "http://88.200.63.148:3001";
+
